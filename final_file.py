734302ca407f28f8b6d7755c3c2fd69c12f9c1ca
rschlek <rschlekewey@gmail.com>
2022-01-27 14:27:12 -0800

    added final file

--- final_file.py
+++ final_file.py
@@ -4,6 +4,10 @@ import copy
 def err(*args):
     sys.stderr.write(', '.join([str(arg) for arg in args]) + "\n")
 
+
+'''
+Class to track the board/state of the game
+'''
 class state:
     def __init__(self, rows, width, height, board=None):
         if board is not None:
@@ -69,3 +73,38 @@ class state:
                 closest_coord = self.pellet_locations[i]
         
         return closest_coord
+
+# width: size of the grid
+# height: top left corner is (x=0, y=0)
+width, height = [int(i) for i in input().split()]
+rows = []
+for i in range(height):
+    row = input()
+    rows.append(row)    # one line of the grid: space " " is floor, pound "#" is wall
+
+game = state(rows,width,height)
+
+# game loop
+while True:
+    my_score, opponent_score = [int(i) for i in input().split()]
+    visible_pac_count = int(input())  # all your pacs and enemy pacs in sight
+    for i in range(visible_pac_count):
+        inputs = input().split()
+        pac_id = int(inputs[0])  # pac number (unique within a team)
+        mine = inputs[1] != "0"  # true if this pac is yours
+        x = int(inputs[2])  # position in the grid
+        y = int(inputs[3])  # position in the grid
+        type_id = inputs[4]  # unused in wood leagues
+        speed_turns_left = int(inputs[5])  # unused in wood leagues
+        ability_cooldown = int(inputs[6])  # unused in wood leagues
+    visible_pellet_count = int(input())  # all pellets in sight
+    for i in range(visible_pellet_count):
+        # value: amount of points this pellet is worth
+        x, y, value = [int(j) for j in input().split()]
+        if value > 1:
+            game.update_board('O',)
+
+
+    # Everything above this are inputs from the game
+
+
