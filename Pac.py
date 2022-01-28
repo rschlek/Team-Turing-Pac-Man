8404e7d0d122a89ceb94f3637788c55dd21e0117
THESTICK16 <98430718+THESTICK16@users.noreply.github.com>
2022-01-27 19:58:40 -0800

    Update Pac.py
    Updated Pac with documentation for functions

--- Pac.py
+++ Pac.py
@@ -25,60 +25,141 @@ class Pac:
         self.ability_cooldown = ability_cooldown
         self.speed_turns_left = speed_turns_left
         self.attacking = False
-       # self.next_destination = next_destination
+        self.see_super_pellet = False
+        self.next_destination = None
+        self.ability_meter_full = False
+        self.rock = "ROCK"
+        self.paper = "PAPER"
+        self.scissors = "SCISSORS"
 
+    '''
+    controls the behavior of the Pac automatically provided Pac parameters are updated properly
+    '''
     def play(self, enemy = None):  # to be called every turn by the gameplayloop in main, this way only one function call must be done on the object and all logic for each pac will run internally
-        self.update_ability_meter()
+        if self.ability_cooldown == 0:
+            self.ability_meter_full = True
+        elif self.ability_cooldown != 0:
+            self.ability_meter_full = False
 
         if self.attacking:
-            self.attack_mode(enemy)
-        else:
-            self.gather_mode()
+            pass
 
-    def attack_mode(self, enemy):
-        if self.pac_chasing is not None:
-            next_location = self.id_num + " " + self.pac_chasing.position.x + " " + self.pac_chasing.position.y
+        elif not self.attacking:
+            pass
+
+    '''
+    prints the next move the Pac is going to take
+    '''
+    def next_move(self, next_move: str):
+        print(next_move)
+
+    '''
+    controls the Pac if an enemy is in sight
+    '''
+    def attack_mode(self, enemy_x = -1, enemy_y = -1, enemy_type = None):
+        if enemy_x == -1:
+            self.attacking = False
+            return
+        else:
+            next_type = self.switch_to(self.type_id, enemy_type)
+            if self.ability_meter_full:
+                if next_type is not None:
+                    self.switch(next_type)
+                    #self.next_move("SWITCH " + self.id_num + " " + next_type)
+                    return
+                else:
+                    self.speed_boost()
+                    return
+
+            next_location = self.id_num + " " + enemy_x + " " + enemy_y
             if self.next_destination != next_location:
                 next_destination = next_location
-                print(next_destination)
+                #self.next_move("MOVE " + self.id_num + " " + next_destination)
+                self.move(enemy_x, enemy_y)
+                return
+
+    '''
+    determines the type that the Pac with 'my_type' should switch to given 'enemy_type'
+    Note: this does not actually swith the pac, that is called from the 'switch' function
+    '''
+    def switch_to(self, my_type: str, enemy_type: str) -> str:
+        if my_type == self.rock:
+            if enemy_type == self.paper:
+                return self.scissors
+            elif enemy_type == self.rock:
+                return self.paper
+            elif enemy_type == self.scissors:
+                return None
+        elif my_type == self.scissors:
+            if enemy_type == self.paper:
+                return None
+            elif enemy_type == self.rock:
+                return self.paper
+            elif enemy_type == self.scissors:
+                return self.rock
+        elif my_type == self.paper:
+            if enemy_type == self.paper:
+                return self.scissors
+            elif enemy_type == self.rock:
+                return None
+            elif enemy_type == self.scissors:
+                return self.rock
+
 
     def gather_mode(self):
         pass
 
-    def update_ability_meter(self):
+    def update_ability_meter(self): #FIXME
         if self.ability_cooldown == 0:
             self.meter_full()
-            self.ability_cooldown = 10
+            self.ability_cooldown = 9
         else:
             self.ability_cooldown -= 1
 
-    def meter_full(self):
+    def meter_full(self): #FIXME
         if self.attacking:
             self.change_type()
         else:
             self.speed_boost()
 
-    def change_type(self):
-        pass
-
-    def speed_boost(self):
-        print(self.id_num + " SPEED")
-
-    def get_location(self):  # -> Vector2:
+    def get_location(self):  # -> Vector2: #FIXME
         return self.location
 
-    def set_location(self, x, y):
+    def set_location(self, x, y): #FIXME
         self.location.x = x
         self.location.y = y
 
     def is_stuck(self) -> bool:
         return False
 
+    '''
+    called from the gameloop when an enemy is in sight.
+    @:param enemy: a Pac object of the enemy Pac
+    '''
     def see_enemy(self, enemy):
         self.pac_chasing = enemy
         self.attacking = True
+    '''
+    sets the next location the Pac will move to and calls the next_move function to print it
+    '''
+    def move(self, x: int, y: int):
+        self.next_destination = x + " " + y
+        self.next_move("MOVE " + self.id_num + " " + x + " " + y)
 
-    def check_best_path(self, position, board = None):
+    '''
+    sets the next type the Pac will switch to and calls the next_move function to print it
+    '''
+    def switch(self, switch_to: str):
+        self.next_move("SWITCH " + self.type_id + " " + switch_to)
+        self.type_id = switch_to  # Remove this if type_id is being updated in the game loop FIXME
+
+    '''
+    calls next_move to print a speed boost call
+    '''
+    def speed_boost(self):
+        self.next_move("SPEED " + self.id_num)
+
+    def check_best_path(self, position, board = None): #FIXME
         if board is not None:
             x = position.x
             y = position.y
@@ -166,10 +247,4 @@ class Pac:
             if right_pellets >= up_pellets and right_pellets >= down_pellets and right_pellets >= left_pellets:
                 return "RIGHT"
 
-
-class Vector2:
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
 
