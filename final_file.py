fb5fd5c06b87eb20da1e263b866401f9379430c2
rschlek <rschlekewey@gmail.com>
2022-01-28 01:54:28 -0800

    working!!!!! kinda

--- final_file.py
+++ final_file.py
@@ -8,7 +8,6 @@ def err(*args):
 
 
 
-
 class Pac:
 
     #    id_num
@@ -26,158 +25,197 @@ class Pac:
     def __init__(self, id_num, type_id, X: int, Y: int, friendly: bool, ability_cooldown: int, speed_turns_left: int):
         self.id_num = id_num
         self.type_id = type_id
-        self.location = (X,Y)
+        self.x = X
+        self.y = Y
         self.friendly = friendly
         self.ability_cooldown = ability_cooldown
         self.speed_turns_left = speed_turns_left
         self.attacking = False
-       # self.next_destination = next_destination
+        self.chasing_super_pellet = False
+        self.next_destination = None
+        self.pac_chasing = None
+        self.ability_meter_full = False
+        self.rock = "ROCK"
+        self.paper = "PAPER"
+        self.scissors = "SCISSORS"
+        self.command = ""
 
-    def play(self, enemy = None):  # to be called every turn by the gameplayloop in main, this way only one function call must be done on the object and all logic for each pac will run internally
-        self.update_ability_meter()
-
-        if self.attacking:
-            self.attack_mode(enemy)
-        else:
-            self.gather_mode()
-
-    def attack_mode(self, enemy):
-        if self.pac_chasing is not None:
-            next_location = self.id_num + " " + self.pac_chasing.position.x + " " + self.pac_chasing.position.y
-            if self.next_destination != next_location:
-                next_destination = next_location
-                print(next_destination)
-
-    def gather_mode(self):
-        pass
-
-    def update_ability_meter(self):
+    '''
+    controls the behavior of the Pac automatically provided Pac parameters are updated properly
+    '''
+    def play(self, board, enemy = None):  # to be called every turn by the gameplayloop in main, this way only one function call must be done on the object and all logic for each pac will run internally
+        self.command = ''
         if self.ability_cooldown == 0:
-            self.meter_full()
-            self.ability_cooldown = 10
-        else:
-            self.ability_cooldown -= 1
+            self.ability_meter_full = True
+        elif self.ability_cooldown != 0:
+            self.ability_meter_full = False
 
-    def meter_full(self):
-        if self.attacking:
-            self.change_type()
-        else:
-            self.speed_boost()
-
-    def change_type(self):
-        pass
-
-    def speed_boost(self):
-        print(str(self.id_num) + " SPEED")
-
-    def get_location(self):  # -> Vector2:
-        return self.location
-
-    def set_location(self, x, y):
-        self.location = (x,y)
         
+        if self.attacking: #make sure to set attacking to false and pac_chasing to None in the game loop if no enemy Pac is in sight #FIXME
+            if self.pac_chasing is not None:
+                self.attack_mode(self.pac_chasing.x, self.pac_chasing.y, self.pac_chasing.type_id)
+            else: self.attacking = False
+            return
+        
+        elif not self.attacking:
+            if not self.chasing_super_pellet: #set to false if super pellet is reached or in game loop
+                closest_pellet = board.closest_pellet(self.y, self.x)
+                next_x = closest_pellet[1]
+                next_y = closest_pellet[0]
+                # for i in closest_pellet:
+                #     if i is int:
+                #         if next_x > -1:
+                #             next_x = i
+                #         elif next_y > -1:
+                #             next_y = i
+                self.move(next_x, next_y)
+                return
+
+        rand_unexplored = board.random_unexplored()
+        if rand_unexplored is not None:
+            next_x = rand_unexplored[1]
+            next_y = rand_unexplored[0]
+            self.move(next_x, next_y)
+            return
+
+        next_x = random.randint(0, board.width - 1)
+        next_y = random.randint(0, board.height - 1)
+        while board[next_x][next_y] == '#':
+            next_x = random.randint(0, board.width - 1)
+            next_y = random.randint(0, board.height - 1)
+        self.move(next_x, next_y)
+
+    '''
+    prints the next move the Pac is going to take
+    '''
+    def next_move(self, next_move: str):
+        self.command = next_move
+
+    '''
+    controls the Pac if an enemy is in sight
+    '''
+    def attack_mode(self, enemy_x = -1, enemy_y = -1, enemy_type = None):
+        if enemy_x == -1:
+            self.attacking = False
+            self.pac_chasing = None
+            return
+        else:
+            next_type = self.switch_to(self.type_id, enemy_type)
+            if self.ability_meter_full:
+                if next_type is not None:
+                    self.switch(next_type)
+                    #self.next_move("SWITCH " + self.id_num + " " + next_type)
+                    return
+                else:
+                    self.speed_boost()
+                    return
+
+            next_location = enemy_x + " " + enemy_y
+            if self.next_destination != next_location:
+                self.next_destination = next_location
+                #self.next_move("MOVE " + self.id_num + " " + next_destination)
+                self.move(enemy_x, enemy_y)
+                return
+
+    '''
+    determines the type that the Pac with 'my_type' should switch to given 'enemy_type'
+    Note: this does not actually swith the pac, that is called from the 'switch' function
+    '''
+    def switch_to(self, my_type: str, enemy_type: str):
+        if my_type == self.rock:
+            if enemy_type == self.paper:
+                return self.scissors
+            elif enemy_type == self.rock:
+                return self.paper
+            elif enemy_type == self.scissors:
+                return None
+        elif my_type == self.scissors:
+            if enemy_type == self.paper:
+                return None
+            elif enemy_type == self.rock:
+                return self.paper
+            elif enemy_type == self.scissors:
+                return self.rock
+        elif my_type == self.paper:
+            if enemy_type == self.paper:
+                return self.scissors
+            elif enemy_type == self.rock:
+                return None
+            elif enemy_type == self.scissors:
+                return self.rock
+
+
+    # def gather_mode(self): #FIXME
+    #     pass
+    #
+    # def update_ability_meter(self): #FIXME
+    #     if self.ability_cooldown == 0:
+    #         self.meter_full()
+    #         self.ability_cooldown = 9
+    #     else:
+    #         self.ability_cooldown -= 1
+    #
+    # def meter_full(self): #FIXME
+    #     if self.attacking:
+    #         self.change_type()
+    #     else:
+    #         self.speed_boost()
+    #
+    # def get_location(self):  # -> Vector2: #FIXME
+    #     return self.location
+    #
+    # def set_location(self, x, y): #FIXME
+    #     self.location.x = x
+    #     self.location.y = y
+
 
     def is_stuck(self) -> bool:
         return False
 
-    def see_enemy(self, enemy):
-        self.pac_chasing = enemy
-        self.attacking = True
+    '''
+    called from the gameloop when an enemy is in sight.
+    @:param enemy: a Pac object of the enemy Pac
+    '''
+    def see_enemy(self, board):
+       # if board.board[][]
+       #  self.pac_chasing = enemy
+        x = self.x
+        y = self.y
 
-    def check_best_path(self, position, board = None):
-        if board is not None:
-            x = position.x
-            y = position.y
-            up_pellets, down_pellets, left_pellets, right_pellets = 0, 0, 0, 0
-            up = ""
-            down = ""
-            left = ""
-            right = ""
-
-            index = board[position.x][position.y]
-            while index != '#':
-                y -= 1
-                index = board[x][y]
-                if index != '#':
-                    up += index + " "
-
-            index = board[position.x][position.y]
-            x = position.x
-            y = position.y
-            while index != '#':
-                y += 1
-                index = board[x][y]
-                if index != '#':
-                    down += index + " "
-
-            index = board[position.x][position.y]
-            x = position.x
-            y = position.y
-            while index != '#':
-                x -= 1
-                index = board[x][y]
-                if index != '#':
-                    left += index + " "
-
-            index = board[position.x][position.y]
-            x = position.x
-            y = position.y
-            while index != '#':
-                x += 1
-                index = board[x][y]
-                if index != '#':
-                    right += index + " "
-
-            for i in up:
-                if i == 'o':
-                    up_pellets += 1
-                elif i == 'O':
-                    up_pellets += 10
-                elif i == 'e':
-                    return "UP"
-
-            for i in down:
-                if i == 'o':
-                    down_pellets += 1
-                elif i == 'O':
-                    down_pellets += 10
-                elif i == 'e':
-                    return "DOWN"
-
-            for i in left:
-                if i == 'o':
-                    left_pellets += 1
-                elif i == 'O':
-                    left_pellets += 10
-                elif i == 'e':
-                    return "LEFT"
-
-            for i in right:
-                if i == 'o':
-                    right_pellets += 1
-                elif i == 'O':
-                    right_pellets += 10
-                elif i == 'e':
-                    return "RIGHT"
-
-            if up_pellets >= down_pellets and up_pellets >= left_pellets and up_pellets >= right_pellets:
-                return "UP"
-
-            if down_pellets >= up_pellets and down_pellets >= left_pellets and down_pellets >= right_pellets:
-                return "DOWN"
-
-            if left_pellets >= up_pellets and left_pellets >= down_pellets and left_pellets >= right_pellets:
-                return "LEFT"
-
-            if right_pellets >= up_pellets and right_pellets >= down_pellets and right_pellets >= left_pellets:
-                return "RIGHT"
+        while board.board[x][y] != '#':
+            if board.board[x][y] == 'e':
+                self.attacking = True
+                self.pac_chasing = (x, y)
+                return
 
 
-class Vector2:
+    def see_super_pellet(self, pellet_x, pellet_y):
+        self.chasing_super_pellet = True
+        if self.x == pellet_x and self.y == pellet_y:
+            self.chasing_super_pellet
+        if self.next_destination != pellet_x + " " + pellet_y:
+            self.next_destination = pellet_x + " " + pellet_y
+            self.move(pellet_x, pellet_y)
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    '''
+    sets the next location the Pac will move to and calls the next_move function to print it
+    '''
+    def move(self, x: int, y: int):
+        self.next_destination = str(x) + " " + str(y)
+        self.next_move("MOVE " + str(self.id_num) + " " + str(x) + " " + str(y))
+
+    '''
+    sets the next type the Pac will switch to and calls the next_move function to print it
+    '''
+    def switch(self, switch_to: str):
+        self.next_move("SWITCH " + str(self.type_id) + " " + switch_to)
+        self.type_id = switch_to  # Remove this if type_id is being updated in the game loop FIXME
+
+    '''
+    calls next_move to print a speed boost call
+    '''
+    def speed_boost(self):
+        self.next_move("SPEED " + str(self.id_num))
 
 
 
@@ -210,13 +248,15 @@ class state:
         for i in range(height):
             for j in range(width):
                 if self.board[i][j] != '#':
-                    self.available_coords.append((j,i))
+                    self.available_coords.append((i,j))
 
         '''
         Stores the locations of known pellets
         '''
         self.pellet_locations = []
         self.explored_squares = []
+
+        self.visited = []
 
     '''
     Updates the board at a certain coordinate with a symbol
@@ -304,7 +344,7 @@ class state:
         if self.board[height][width] == '#':
             err('Invalid coordinate (add_pellet)')
         else:
-            self.pellet_locations.append((height,width))
+            self.pellet_locations.append((width,height))
 
 
     
@@ -329,6 +369,7 @@ game = state(rows,width,height)
 first = True
 our_pacs = []
 turn_command = ''
+temp = 0
 
 # game loop
 while True:
@@ -353,11 +394,25 @@ while True:
             our_pacs.append(pac)
             game.add_explored(x,y)
         if mine and not first:
-            our_pacs[pac_id].x = x
-            our_pacs[pac_id].y = y
-            our_pacs[pac_id].speed_turns_left = speed_turns_left
-            our_pacs[pac_id].ability_cooldown = ability_cooldown
+            our_pacs[temp].x = x
+            our_pacs[temp].y = y
+            our_pacs[temp].speed_turns_left = speed_turns_left
+            our_pacs[temp].ability_cooldown = ability_cooldown
+            our_pacs[temp].type_id = type_id
+            game.add_explored(x,y)
+            temp += 1
+        if not mine and not first:
+            enemy = Pac(pac_id,type_id,x,y,True,ability_cooldown,speed_turns_left)
 
+        game.update_board(' ',x,y)
+        if (x,y) in game.pellet_locations:   
+            err('Got Here deleting ', (x,y))
+            game.pellet_locations.remove((x,y))
+
+
+    temp = 0
+
+    first = False
 
     visible_pellet_count = int(input())  # all pellets in sight
     for i in range(visible_pellet_count):
@@ -373,7 +428,9 @@ while True:
             game.add_pellet(x,y)
 
     for pac in our_pacs:
-        pac.play()
-        turn_command += pac.command() + '|'
+        pac.play(game)
+        turn_command += pac.command + '|'
 
     print(turn_command)
+    turn_command = ''
+    
